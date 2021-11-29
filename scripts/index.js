caf2a61fe00aa6a63acbba0298bb17268a3aad49
vitaly lukyanchenko <vit123@list.ru>
2021-11-29 16:03:13 +0200

    adds animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,17 @@
 const thumbnails = document.querySelectorAll(".thumbnails-anchore");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const detailsContainer = document.querySelector(".details-container");
 const mainContent = document.querySelector(".main-class");
 // тип данных let нужен для того, чтобы можно было менять значение переменной
 let selectedItem = thumbnails[0].parentElement;
 const HIDDEN = "hidden";
-const SELECTED ="selected"
+const SELECTED ="selected";
+const IS_TINY = "isTiny";
 
 thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function(event) {
+    //    отменяем стандартное поведение ссылки: открытие в новом окне
         event.preventDefault();
         showDetails();
         setDetails(thumbnail);
@@ -19,10 +22,13 @@ function setDetails(thumbnail) {
     detailsImage.src = thumbnail.href;
     // получение контента из элемента делается через getter
     detailsTitle.textContent = thumbnail.getAttribute("data-detailed-title");
+    detailsContainer.classList.add(IS_TINY);
+    setTimeout(() => detailsContainer.classList.remove(IS_TINY), 0);
 }
 function showDetails() {
     // методы группы classList не требуют указания точки (.) перед названием класса
     mainContent.classList.remove(HIDDEN);
+
 }
 function hideDetails() {
     mainContent.classList.add(HIDDEN);
